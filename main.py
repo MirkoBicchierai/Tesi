5cbd0739e447e62dc9f41fbbaf68ccebafc70765
mirko <bicchierai.mirko.developer@gmail.com>
2023-06-02 17:54:14 +0200

    some fix

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import torch.nn.functional as F
 from DataLoader import FastDataset
 from Model import DecoderRNN
+from test import plot_graph
 
 
 def main():
@@ -13,24 +14,28 @@ def main():
     test_path = "Landmark_dataset/dataset_testing/Complete"  # Partial
     save_path = "Models/"
 
+    filelist = [f for f in os.listdir("Graph/")]
+    for f in filelist:
+        os.remove(os.path.join("Graph/", f))
+
     dataset_train = FastDataset(train_path)
-    training_dataloader = DataLoader(dataset_train, batch_size=40, shuffle=True, drop_last=False)
+    training_dataloader = DataLoader(dataset_train, batch_size=256, shuffle=True, drop_last=False)
     dataset_test = FastDataset(test_path)
-    testing_dataloader = DataLoader(dataset_test, batch_size=1, shuffle=True, drop_last=False)
+    testing_dataloader = DataLoader(dataset_test, batch_size=2, shuffle=False, drop_last=False)
 
     hidden_size = 256
-    num_classes = 10  # number of faces
+    num_classes = 70  # number of label
     output_size = (68 * 3)  # landmark point * coordinates
     frame_generate = 60  # number of frame generate by lstm
 
     model = DecoderRNN(hidden_size, output_size, num_classes, frame_generate, device).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 
     epochs = 500
     for epoch in tqdm(range(epochs)):
         tot_loss = 0
         for landmark_animation, label, str_label in training_dataloader:
-            for idF, frame in enumerate(landmark_animation[:, 1:]):
+            for idF in range(landmark_animation[:, 1:].shape[1]):
                 optimizer.zero_grad()
                 landmark_animation = landmark_animation.type(torch.FloatTensor).to(device)
                 output = model(landmark_animation[:, idF], label, 60 - idF)
@@ -45,17 +50,21 @@ def main():
         if not (epoch + 1) % 50:
             tot_loss_test = 0
             model.eval()
+            check = True
             for landmark_animation, label, str_label in testing_dataloader:
                 landmark_animation = landmark_animation.type(torch.FloatTensor).to(device)
                 with torch.no_grad():
                     output = model(landmark_animation[:, 0], label, 60)
+                    if check:
+                        plot_graph(output.cpu().numpy(), str_label, epoch)
+                    check = False
                 test_loss = F.mse_loss(output, landmark_animation[:, 1:])
                 tot_loss_test += test_loss.item()
 
             print("Epoch: ", epoch + 1, " - Testing loss: ", tot_loss_test / len(testing_dataloader))
             model.train()
 
-    torch.save(model, os.path.join(save_path, "modelComplete.pt"))
+    torch.save(model, os.path.join(save_path, "modelComplete.pt"))  # modelPartial.pt
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -10,14 +10,12 @@ import os
 label_faces_check = {}
 
 
-def plot_graph(vector, label):
+def plot_graph(vector, label, epoch):
     for i in range(vector.shape[0]):
         if label[i] not in label_faces_check:
             label_faces_check[label[i]] = 1
-            actual_face = 1
         else:
             label_faces_check[label[i]] = label_faces_check[label[i]] + 1
-            actual_face = label_faces_check[label[i]]
 
         png_files = []
 
@@ -37,26 +35,36 @@ def plot_graph(vector, label):
             ax.set_zlabel('Z')
             ax.scatter(x, y, z)
             ax.view_init(90, -90)
-            png_files.append("Grafici/" + 'faccia_' + str(actual_face) + '_' + label[i] + '_frame_' + str(j) + '.png')
-            plt.savefig("Grafici/" + 'faccia_' + str(actual_face) + '_' + label[i] + '_frame_' + str(j) + '.png')
+            if epoch == -1:
+                p_save = "Graph/" + label[i] + '_frame_' + str(j) + '.png'
+            else:
+                p_save = "Graph/" + 'epoch_' + str(epoch + 1) + '_' + label[i] + '_frame_' + str(j) + '.png'
+
+            png_files.append(p_save)
+            plt.xlim(-0.2, 0.2)
+            plt.ylim(-0.2, 0.2)
+            plt.savefig(p_save)
             plt.close()
 
         frames = []
         for png_file in png_files:
             frame = Image.open(png_file)
             frames.append(frame)
-        gif_filename = 'Grafici/faccia-' + str(actual_face) + '-' + label[i] + '.gif'
+        if epoch == -1:
+            gif_filename = 'Graph/' + label[i] + '.gif'
+        else:
+            gif_filename = 'Graph/epoch' + str(epoch + 1) + "_" + label[i] + '.gif'
         frames[0].save(gif_filename, format="GIF", append_images=frames[1:], save_all=True, duration=120, loop=0)
 
 
 def main():
-    filelist = [f for f in os.listdir("Grafici/")]
+    filelist = [f for f in os.listdir("Graph/")]
     for f in filelist:
-        os.remove(os.path.join("Grafici/", f))
+        os.remove(os.path.join("Graph/", f))
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     test_path = "Landmark_dataset/dataset_testing/Partial"
-    save_path = "Models/modelPartial.pt"
+    save_path = "Models/modelPartial.pt"  # modelComplete.pt
 
     dataset_test = FastDataset(test_path)
     testing_dataloader = DataLoader(dataset_test, batch_size=10, shuffle=True, drop_last=False)
@@ -67,7 +75,7 @@ def main():
         landmark_animation = landmark_animation.type(torch.FloatTensor).to(device)
         with torch.no_grad():
             output = model(landmark_animation[:, 0], label, 60)
-            plot_graph(output.cpu().numpy(), str_label)
+            plot_graph(output.cpu().numpy(), str_label, epoch=-1)
 
     print(label_faces_check)
 
