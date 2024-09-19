b6ce0307fb62f51acf9ba6a008712bc48e384820
mirko <bicchierai.mirko.developer@gmail.com>
2024-09-19 16:38:41 +0200

    Reset

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
         test_path = "Landmark_dataset_flame_aligned_coma/dataset_testing"
         actors_path = "Actors_Coma/"
         type_dataset = "COMA"
-        hidden_size = 2048
+        hidden_size = 2049
         num_classes = 12
         output_size = (68 * 3)
         frame_generate = 40
